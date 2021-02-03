8f2faaf91de4d9fe66078768d34f6d97587521c0
Wino.Ltd <72581300+winoproject@users.noreply.github.com>
2021-02-03 09:52:30 -0800

    Bring -Up : Shiva - Fluid

---
+++ BoardConfig.mk
@@ -0,0 +1,163 @@
+#
+# Copyright (C) 2019 Potato Open Sauce Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/xiaomi/shiva
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# APEX image
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
+# Audio
+AUDIO_FEATURE_ENABLED_AAC_ADTS_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_EXTN_FORMATS := true
+AUDIO_FEATURE_ENABLED_HDMI_SPK := true
+AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
+USE_CUSTOM_AUDIO_POLICY := 1
+USE_XML_AUDIO_POLICY_CONF := 1
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := shiva
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6768
+TARGET_BOARD_PLATFORM_GPU := Mali-G52 MC2
+
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.usbconfigfs=true androidboot.selinux=permissive buildvariant=eng sys.usb.configfs=1
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x07c08000
+BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
+BOARD_PREBUILT_OIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := shiva lancelot
+
+# Avb
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
+# Broken rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_PHONY_TARGETS := true
+
+# Network Routing
+TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 614572800
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 114601984000
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 452984832
+BOARD_DTBOIMG_PARTITION_SIZE := 16777216
+TARGET_USES_MKE2FS := true
+
+# Dynamic partitions
+BOARD_SUPER_PARTITION_SIZE := 6012043264
+BOARD_SUPER_PARTITION_GROUPS := main
+BOARD_MAIN_SIZE := 6012043264
+BOARD_MAIN_LIST := \
+    system \
+    product
+
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_PRODUCT := product
+BUILD_WITHOUT_VENDOR := true
+
+# System as root
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# File systems
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6768
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2020-09-01
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+ODM_MANIFEST_SKUS += \
+    shiva
+
+ODM_MANIFEST_SHIVA_FILES := $(DEVICE_PATH)/manifest_shiva.xml
+
+# Treble
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_VNDK_VERSION := current
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+PRODUCT_VENDOR_MOVE_ENABLED := true
+BOARD_VNDK_RUNTIME_DISABLE := true
+
+# Sepolicy
+#include device/mediatek/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true 
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/shiva/BoardConfigVendor.mk
---
+++ fluid_shiva.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2019 Potato Open Sauce Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := shiva
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_q.mk)
+
+# Inherit from shiva device
+$(call inherit-product, device/xiaomi/shiva/device.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := shiva
+PRODUCT_NAME := lineage_shiva
+PRODUCT_BRAND := Poco
+PRODUCT_MODEL := Poco M2
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Poco/shiva_global/shiva:10/QP1A.190711.020/V11.0.8.0.QJCMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="shiva-user 10 QP1A.190711.020 release-keys" \
+    PRODUCT_NAME="shiva" \
+    TARGET_DEVICE="shiva"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
